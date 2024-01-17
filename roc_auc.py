2374a8c76f49324a098852bced1174077f88fee0
Heshy Roskes <heshyr@gmail.com>
2024-01-17 11:30:40 -0500

    making guess work

--- roc_auc.py
+++ roc_auc.py
@@ -1,9 +1,10 @@
 import numpy as np, scipy.integrate
 
-def optimize(X, Y, Xdot, Ydot, AUC):
+def optimize(X, Y, Xdot, Ydot, AUC, Lambda_guess, Lambda_scaling=1):
   def fun(t, xy, params):
     x, y = xy
     Lambda, c1, c2 = params
+    Lambda *= Lambda_scaling
 
     xdot = 2 * Xdot(t) / (+Lambda * y + c1)
     ydot = 2 * Ydot(t) / (-Lambda * x + c2)
@@ -14,16 +15,18 @@ def optimize(X, Y, Xdot, Ydot, AUC):
     xminusinfinity, yminusinfinity = xyminusinfinity
     xplusinfinity, yplusinfinity = xyplusinfinity
     Lambda, c1, c2 = params
+    Lambda *= Lambda_scaling
+
     bcs = [xminusinfinity, yminusinfinity, xplusinfinity-1, yplusinfinity-1, Lambda * AUC + c1 - 2, -Lambda * (1-AUC) + c2 - 2]
     return np.asarray(bcs[1:])
 
   t_plot = np.linspace(-10, 10, 1001)
 
+  Lambda_guess /= Lambda_scaling
   xy_guess = np.asarray([X(t_plot), Y(t_plot)])
-  Lambda_guess = 1
   c1_guess = 2 - Lambda_guess * AUC
   c2_guess = 2 + Lambda_guess * (1-AUC)
   params_guess = np.array([Lambda_guess, c1_guess, c2_guess])
 
-  return scipy.integrate.solve_bvp(fun=fun, bc=bc, x=t_plot, y=xy_guess, p=params_guess, max_nodes=100000)
+  return scipy.integrate.solve_bvp(fun=fun, bc=bc, x=t_plot, y=xy_guess, p=params_guess, max_nodes=1000000)
 
