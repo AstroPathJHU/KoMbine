03e9f3a437b830e61aaea471c3b1c2628090df58
Heshy Roskes <heshyr@gmail.com>
2024-01-17 12:59:41 -0500

    better guess if you're not too far off the real AUC

--- roc_auc.py
+++ roc_auc.py
@@ -21,12 +21,61 @@ def optimize(X, Y, Xdot, Ydot, AUC, Lambda_guess, Lambda_scaling=1):
     return np.asarray(bcs[1:])
 
   t_plot = np.linspace(-10, 10, 1001)
+  guess = xy_guess(X=X, Y=Y, t_plot=t_plot, AUC=AUC)
 
   Lambda_guess /= Lambda_scaling
-  xy_guess = np.asarray([X(t_plot), Y(t_plot)])
   c1_guess = 2 - Lambda_guess * AUC
   c2_guess = 2 + Lambda_guess * (1-AUC)
   params_guess = np.array([Lambda_guess, c1_guess, c2_guess])
 
-  return scipy.integrate.solve_bvp(fun=fun, bc=bc, x=t_plot, y=xy_guess, p=params_guess, max_nodes=1000000)
+  result = scipy.integrate.solve_bvp(fun=fun, bc=bc, x=t_plot, y=guess, p=params_guess, max_nodes=1000000)
+  print(bc(result.y[:,0], result.y[:,-1], result.p))
+  return result
 
+def xy_guess(X, Y, t_plot, AUC):
+  if not 0 <= AUC <= 1:
+    raise ValueError(f"AUC={AUC} is not between 0 and 1")
+
+  X = X(t_plot)
+  Y = Y(t_plot)
+  XplusY = X + Y
+  XminusY = X - Y
+
+  xplusy = XplusY
+
+  def xminusy_s(s):
+    max_allowed_xminusy = np.min([xplusy, 2 - xplusy], axis=0)
+    min_allowed_xminusy = -max_allowed_xminusy
+
+    if s >= 0:
+      xminusy = XminusY * (1-s) + min_allowed_xminusy * s
+    elif s < 0:
+      xminusy = XminusY * (1+s) + max_allowed_xminusy * (-s)
+
+    return xminusy
+
+  def AUCresidual_s(s):
+    xminusy = xminusy_s(s)
+    x = (xplusy + xminusy) / 2
+    y = (xplusy - xminusy) / 2
+
+    AUC_s = 1/2 * np.sum((x[1:] - x[:-1]) * (y[1:] + y[:-1]))
+    return AUC_s - AUC
+
+  np.testing.assert_allclose(AUCresidual_s(1), 1-AUC, atol=1e-5, rtol=0)
+  np.testing.assert_allclose(AUCresidual_s(-1), -AUC, atol=1e-5, rtol=0)
+
+  if AUC == 1:
+    s = 1
+  elif AUC == 0:
+    s = -1
+  else:
+    result = scipy.optimize.root_scalar(AUCresidual_s, method="bisect", bracket=[-1, 1])
+    if not result.converged:
+      raise RuntimeError("Optimize didn't converge")
+    s = result.root
+
+  xminusy = xminusy_s(s)
+  x = (xplusy + xminusy) / 2
+  y = (xplusy - xminusy) / 2
+  return np.array([x, y])
