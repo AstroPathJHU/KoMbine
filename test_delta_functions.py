061a765f5a4163a3be6fbf2a64ac005662c72de7
Heshy Roskes <heshyr@gmail.com>
2024-03-19 12:05:30 -0400

    this one actually works

--- test_delta_functions.py
+++ test_delta_functions.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt, numpy as np
 import delta_functions
 
-responders = [-5, -4, -3, -2, -1, 0, 1, 2, 3]
-nonresponders = [-0.5, 0.5, 1.5, 1.7, 2.5, 2.7, 2.8, 3.5, 4, 5, 6, 7]
+responders = np.linspace(-10, 10, 21)
+nonresponders = responders+.5
 
 def plot_params(responders, nonresponders, *, skip_aucs=[]):
   target_aucs = []
@@ -28,6 +28,7 @@ def plot_params(responders, nonresponders, *, skip_aucs=[]):
     [AUC] + [_ for _ in np.linspace(1, 0, 21) if _ <= AUC],
   ]
 
+  plt.figure(figsize=(5, 5))
   for linspace in linspaces:
     last_failed = False
     for target_auc in linspace:
@@ -39,8 +40,6 @@ def plot_params(responders, nonresponders, *, skip_aucs=[]):
       xx = x(t)
       yy = y(t)
       auc = 1/2 * np.sum((yy[1:]+yy[:-1]) * (xx[1:] - xx[:-1]))
-      plt.scatter(xx, yy)
-      plt.show()
       delta_auc = auc - target_auc
       if abs(delta_auc) > 1e-4 or abs(xx[-1]-1) > 1e-4 or abs(yy[-1]-1) > 1e-4:
         print("failed", target_auc)
@@ -50,11 +49,14 @@ def plot_params(responders, nonresponders, *, skip_aucs=[]):
           last_failed = True
           continue
       last_failed = False
+      plt.scatter(xx, yy, label=f"{target_auc:.3g}")
       target_aucs.append(target_auc)
       delta_aucs.append(delta_auc)
       c1.append(result.c1)
       c5.append(result.c5)
       L.append(result.Lambda)
+  plt.legend()
+  plt.show()
   plt.figure(figsize=(5, 5))
   plt.scatter(target_aucs, c1, label="$c_{1}$")
   plt.scatter(target_aucs, c5, label="$c_{5}$")
