4ed342d3d8384006f34588cfa17e2061349280c9
Heshy Roskes <heshyr@gmail.com>
2024-04-16 11:50:10 -0400

    working on docs

--- roc_picker/discrete.py
+++ roc_picker/discrete.py
@@ -9,60 +9,60 @@ class Discrete:
   def ts(self):
     return sorted(set(self.responders) | set(self.nonresponders))
   @functools.cached_property
-  def Xdot(self):
+  def Xscr(self):
     return collections.Counter(self.nonresponders)
   @functools.cached_property
-  def Ydot(self):
+  def Yscr(self):
     return collections.Counter(self.responders)
 
-  def checkvalidity(self, xdot, ydot):
-    for t in xdot:
-      if xdot[t] != 0 and self.Xdot[t] == 0:
-        raise ValueError(f"xdot has nonzero at t={t} but Xdot does not")
-    for t in ydot:
-      if ydot[t] != 0 and self.Ydot[t] == 0:
-        raise ValueError(f"ydot has nonzero at t={t} but Ydot does not")
-    np.testing.assert_allclose([sum(xdot.values()), sum(ydot.values())], 1)
+  def checkvalidity(self, xscr, yscr):
+    for t in xscr:
+      if xscr[t] != 0 and self.Xscr[t] == 0:
+        raise ValueError(f"xscr has nonzero at t={t} but Xscr does not")
+    for t in yscr:
+      if yscr[t] != 0 and self.Yscr[t] == 0:
+        raise ValueError(f"yscr has nonzero at t={t} but Yscr does not")
+    np.testing.assert_allclose([sum(xscr.values()), sum(yscr.values())], 1)
 
-  def evalNLL(self, xdot, ydot):
-    self.checkvalidity(xdot, ydot)
+  def evalNLL(self, xscr, yscr):
+    self.checkvalidity(xscr, yscr)
     NLL = 0
     for t in self.ts:
-      if self.Xdot[t] != 0:
-        NLL -= self.Xdot[t] * np.log(xdot[t])
-      if self.Ydot[t] != 0:
-        NLL -= self.Ydot[t] * np.log(ydot[t])
+      if self.Xscr[t] != 0:
+        NLL -= self.Xscr[t] * np.log(xscr[t])
+      if self.Yscr[t] != 0:
+        NLL -= self.Yscr[t] * np.log(yscr[t])
     return NLL
 
-  def buildroc(self, xdot, ydot):
-    self.checkvalidity(xdot, ydot)
+  def buildroc(self, xscr, yscr):
+    self.checkvalidity(xscr, yscr)
     x = np.zeros(shape=len(self.ts)+2)
     y = np.zeros(shape=len(self.ts)+2)
     for i, t in enumerate([-np.inf] + self.ts + [np.inf]):
-      x[i] = sum(v for k, v in xdot.items() if k < t)
-      y[i] = sum(v for k, v in ydot.items() if k < t)
+      x[i] = sum(v for k, v in xscr.items() if k < t)
+      y[i] = sum(v for k, v in yscr.items() if k < t)
       if x[-1]:
         x /= x[-1]
       if y[-1]:
         y /= y[-1]
     return x, y
 
-  def evalAUC(self, xdot, ydot):
-    xx, yy = self.buildroc(xdot, ydot)
+  def evalAUC(self, xscr, yscr):
+    xx, yy = self.buildroc(xscr, yscr)
     return np.sum(0.5 * (xx[1:] - xx[:-1]) * (yy[1:] + yy[:-1]))
 
   def optimize(self, AUC=None):
     def unpackxy(xy):
-      np.testing.assert_equal(len(xy), sum((self.Xdot[t] != 0) + (self.Ydot[t] != 0) for t in self.ts))
+      np.testing.assert_equal(len(xy), sum((self.Xscr[t] != 0) + (self.Yscr[t] != 0) for t in self.ts))
       xy_iterator = iter(xy)
-      xdot = collections.Counter()
-      ydot = collections.Counter()
+      xscr = collections.Counter()
+      yscr = collections.Counter()
       for t in self.ts:
-        if self.Xdot[t] != 0:
-          xdot[t] = next(xy_iterator)
+        if self.Xscr[t] != 0:
+          xscr[t] = next(xy_iterator)
       for t in self.ts:
-        if self.Ydot[t] != 0:
-          ydot[t] = next(xy_iterator)
+        if self.Yscr[t] != 0:
+          yscr[t] = next(xy_iterator)
       try:
         next(xy_iterator)
       except StopIteration:
@@ -70,39 +70,53 @@ class Discrete:
       else:
         assert False
 
-      xsum = sum(xdot.values())
-      ysum = sum(ydot.values())
-      for k in xdot: xdot[k] /= xsum
-      for k in ydot: ydot[k] /= ysum
+      xsum = sum(xscr.values())
+      ysum = sum(yscr.values())
+      for k in xscr: xscr[k] /= xsum
+      for k in yscr: yscr[k] /= ysum
 
-      return xdot, ydot
+      return xscr, yscr
 
     def NLL(xy):
-      xdot, ydot = unpackxy(xy)
-      return self.evalNLL(xdot, ydot)
+      xscr, yscr = unpackxy(xy)
+      return self.evalNLL(xscr, yscr)
 
     guess = []
     for t in self.ts:
-      if self.Xdot[t] != 0:
-        guess.append(self.Xdot[t])
+      if self.Xscr[t] != 0:
+        guess.append(self.Xscr[t])
     for t in self.ts:
-      if self.Ydot[t] != 0:
-        guess.append(self.Ydot[t])
+      if self.Yscr[t] != 0:
+        guess.append(self.Yscr[t])
 
-    constraints = []
+    def sumxscr(xy):
+      xscr, yscr = unpackxy(xy)
+      return sum(xscr) - 1
+    def sumyscr(xy):
+      xscr, yscr = unpackxy(xy)
+      return sum(yscr) - 1
+    constraints = [
+      #{
+      #  "type": "eq",
+      #  "fun": sumxscr,
+      #}, {
+      #  "type": "eq",
+      #  "fun": sumyscr,
+      #}
+    ]
     if AUC is not None:
       def constraintfunction(xy):
-        xdot, ydot = unpackxy(xy)
-        return self.evalAUC(xdot, ydot) - AUC
+        xscr, yscr = unpackxy(xy)
+        return self.evalAUC(xscr, yscr) - AUC
       constraints.append({
         "type": "eq",
         "fun": constraintfunction,
       })
 
     result = scipy.optimize.minimize(NLL, guess, constraints=constraints, method="SLSQP")
-    result["xdotydot"] = xdotydot = result.pop("x")
-    xdot, ydot = unpackxy(xdotydot)
-    result["x"], result["y"] = self.buildroc(xdot, ydot)
-    result["AUC"] = self.evalAUC(xdot, ydot)
+    result["xscryscr"] = xscryscr = result.pop("x")
+    xscr, yscr = unpackxy(xscryscr)
+    result["x"], result["y"] = self.buildroc(xscr, yscr)
+    result["AUC"] = self.evalAUC(xscr, yscr)
     result["NLL"] = result["fun"]
     return result
