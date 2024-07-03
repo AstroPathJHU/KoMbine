ca66e661539917d9c3920d1c0a4d63d3919b3587
Heshy Roskes <heshyr@gmail.com>
2024-07-03 13:51:14 -0400

    was making the wrong plot

--- docs/compile_plots.sh
+++ docs/compile_plots.sh
@@ -12,6 +12,6 @@ rocpicker_discrete ../test/datacards/simple_examples/datacard_example_1.txt --ro
 rocpicker_discrete ../test/datacards/simple_examples/datacard_example_2.txt --scan-filename discrete_scan_compare_to_delta_functions.pdf --y-upper-limit 20 --npoints 100
 
 rocpicker_delta_functions ../test/datacards/simple_examples/datacard_example_2.txt --roc-filename deltafunctions_exampleroc.pdf --scan-filename deltafunctions_scan.pdf --roc-errors-filename deltafunctions_exampleroc_errors.pdf --y-upper-limit 20 --npoints 100
-rocpicker_delta_functions ../test/datacards/simple_examples/datacard_example_2.txt --scan-filename delta_functions_scan_compare_to_discrete.pdf --y-upper-limit 20 --npoints 100
+rocpicker_delta_functions ../test/datacards/simple_examples/datacard_example_1.txt --scan-filename delta_functions_scan_compare_to_discrete.pdf --y-upper-limit 20 --npoints 100
 
 python ../test/test_continuous_distributions.py
