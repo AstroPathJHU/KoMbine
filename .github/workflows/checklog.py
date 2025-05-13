5a15a3fe028dd93d2da346c476940cd251c477bf
Heshy Roskes <hroskes@jhu.edu>
2025-05-13 11:07:56 -0400

    bug in biblatex-ieee

--- .github/workflows/checklog.py
+++ .github/workflows/checklog.py
@@ -3,11 +3,12 @@ Check the log from latex for warnings and errors.
 """
 
 import argparse
+import re
 import textwrap
 
 from texoutparse import LatexLogParser
 
-def checklatex(filename):
+def checklatex(filename, ignore_regexes=[]):
   """
   Check the log from latex for warnings and errors.
   """
@@ -15,20 +16,24 @@ def checklatex(filename):
   with open(filename, encoding="utf-8") as f:
     p.process(f)
 
+  errors = [e for e in p.errors if not any(r.search(e.info["message"]) for r in ignore_regexes)]
+  warnings = [e for e in p.warnings if not any(r.search(e.info["message"]) for r in ignore_regexes)]
+  badboxes = [e for e in p.badboxes if not any(r.search(e.info["message"]) for r in ignore_regexes)]
+  missing_refs = [e for e in getattr(p, "missing_refs", []) if not any(r.search(e.info["message"]) for r in ignore_regexes)]
+
   message = []
-  if p.errors:
+  if errors:
     message.append(
-      "Errors:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in p.errors)
+      "Errors:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in errors)
     )
-  if p.warnings:
+  if warnings:
     message.append(
-      "Warnings:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in p.warnings)
+      "Warnings:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in warnings)
     )
-  if p.badboxes:
+  if badboxes:
     message.append(
-      "Bad boxes:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in p.badboxes)
+      "Bad boxes:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in badboxes)
     )
-  missing_refs = getattr(p, "missing_refs", [])
   if missing_refs:
     message.append(
       "Bad refs:\n\n" + "\n\n".join(textwrap.indent(str(e), "  ") for e in missing_refs)
@@ -36,7 +41,7 @@ def checklatex(filename):
 
   if message:
     raise RuntimeError(
-      f"Latex gave some {'errors' if p.errors else 'warnings'}:\n\n\n" + "\n\n\n".join(message)
+      f"Latex gave some {'errors' if errors else 'warnings'}:\n\n\n" + "\n\n\n".join(message)
     )
 
 def main(args=None):
@@ -45,8 +50,9 @@ def main(args=None):
   """
   p = argparse.ArgumentParser()
   p.add_argument("filename")
+  p.add_argument("--ignore-regex", action="append", help="Ignore regex", type=re.compile, default=[])
   args = p.parse_args(args=args)
-  checklatex(args.filename)
+  checklatex(args.filename, ignore_regexes=args.ignore_regex)
 
 if __name__ == "__main__":
   main()
