79ae7fe9d3ade437f971cd26c20d06de85137e35
Heshy Roskes <hroskes@jhu.edu>
2025-05-16 15:08:53 -0400

    test the conversion to (x, y) as well

--- test/test_km_systematics_mc.py
+++ test/test_km_systematics_mc.py
@@ -41,16 +41,23 @@ def main():
   kmdistributions = datacard.systematics_mc_km(parameter_min=70, parameter_max=np.inf)
   kmcollection = kmdistributions.generate(size=1000, random_state=123456)
   km_quantiles = kmcollection.survival_probabilities_quantiles(quantiles)
+  #test that (x, y) are the same as the reference
+  #this serves to test that the plotting part of the code works correctly
+  km_quantiles_xy = np.array([
+    kmcollection._points_for_plot(kmcollection.times_for_plot, km)
+    for km in km_quantiles
+  ])
+
 
   tolerance: Tolerance = {"atol": 1e-6, "rtol": 1e-6}
 
   try:
     with open(here/"reference"/"km_systematics_mc.pkl", "rb") as f:
       ref = pickle.load(f)
-      np.testing.assert_allclose(km_quantiles, ref, **tolerance)
+      np.testing.assert_allclose(km_quantiles_xy, ref, **tolerance)
   except:
-    with open(here/"test_output"/"km_systematics.pkl", "wb") as f:
-      pickle.dump(km_quantiles, f)
+    with open(here/"test_output"/"km_systematics_mc.pkl", "wb") as f:
+      pickle.dump(km_quantiles_xy, f)
     raise
 
 if __name__ == "__main__":
