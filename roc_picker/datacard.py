999e60b21738e3c8112fe91150679969d7104fa3
Heshy Roskes <hroskes@jhu.edu>
2025-09-17 10:19:37 -0400

    fix pylance

--- roc_picker/datacard.py
+++ roc_picker/datacard.py
@@ -4,7 +4,7 @@ The main Datacard class now lives in the kombine package.
 """
 
 # Re-export the Datacard class from kombine for backward compatibility
-from kombine.datacard import Datacard
+from kombine.datacard import Datacard, FixedObservable, Patient, PoissonObservable
 
 # Re-export common classes and functions that ROC Picker needs
-__all__ = ['Datacard']
+__all__ = ['Datacard', 'FixedObservable', 'Patient', 'PoissonObservable']
