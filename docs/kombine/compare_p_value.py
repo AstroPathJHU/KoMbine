872bf91f96212dc25ac94577c40ac73ff8614feb
Heshy Roskes <hroskes@jhu.edu>
2025-11-18 09:27:01 -0500

    pylint

--- docs/kombine/compare_p_value.py
+++ docs/kombine/compare_p_value.py
@@ -73,7 +73,7 @@ def simulate_pvalues( #pylint: disable=too-many-locals
 
   return results
 
-def plot_pvalue_comparison( #pylint: disable=too-many-arguments, too-many-locals
+def plot_pvalue_comparison( #pylint: disable=too-many-arguments, too-many-locals, too-many-statements
   pvalues: np.ndarray,
   title: str = "Comparison of p value methods",
   *,
